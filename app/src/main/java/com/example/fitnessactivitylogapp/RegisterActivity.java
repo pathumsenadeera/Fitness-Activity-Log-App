35cfabe29b648970ecae38db3fe2203f7c9b90d2
pathumsenadeera <152371134+pathumsenadeera@users.noreply.github.com>
2026-03-04 08:42:42 +0530

    Implemented form validation and email format checking

--- app/src/main/java/com/example/fitnessactivitylogapp/RegisterActivity.java
+++ app/src/main/java/com/example/fitnessactivitylogapp/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -37,6 +39,28 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        // Set listener for Register button with validations
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etFullName.getText().toString().trim();
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                //Check if any field is empty
+                if(name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+                }
+                //Check if email format is valid
+                else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(RegisterActivity.this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    // Placeholder for Database Insertion (Next Step)
+                }
             }
         });
     }
