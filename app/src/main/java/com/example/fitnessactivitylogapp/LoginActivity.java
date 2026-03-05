dcd9f966d65c83931ba0bbb148955623e4eb1eb8
pathumsenadeera <152371134+pathumsenadeera@users.noreply.github.com>
2026-03-06 00:45:43 +0530

    Setup LoginActivity UI components and database initialization

--- app/src/main/java/com/example/fitnessactivitylogapp/LoginActivity.java
+++ app/src/main/java/com/example/fitnessactivitylogapp/LoginActivity.java
@@ -1,4 +1,43 @@
 package com.example.fitnessactivitylogapp;
 
-public class LoginActivity {
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText etEmail, etPassword;
+    Button btnLogin;
+    TextView tvRegisterRedirect;
+    DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        // Database setup
+        db = new DatabaseHelper(this);
+
+        // UI element linking
+        etEmail = findViewById(R.id.etLoginEmail);
+        etPassword = findViewById(R.id.etLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvRegisterRedirect = findViewById(R.id.tvRegisterRedirect);
+
+        // Basic redirection to Register screen
+        tvRegisterRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
