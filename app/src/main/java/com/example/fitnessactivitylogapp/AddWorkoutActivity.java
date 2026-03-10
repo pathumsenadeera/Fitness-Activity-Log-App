d7173241ffabf51e653541dc6d10f9deb1a5f1ff
DumudiBhagya <ict2022110@as.rjt.ac.lk>
2026-03-10 11:49:00 +0530

    Initialized UI components and DatabaseHelper for AddWorkoutActivity

--- app/src/main/java/com/example/fitnessactivitylogapp/AddWorkoutActivity.java
+++ app/src/main/java/com/example/fitnessactivitylogapp/AddWorkoutActivity.java
@@ -1,7 +1,31 @@
 package com.example.fitnessactivitylogapp;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddWorkoutActivity extends AppCompatActivity {
 
+    // UI Component declarations
+    EditText etType, etValue;
+    Button btnSave;
+    DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_workout_page);
+
+        // Initialize DatabaseHelper
+        db = new DatabaseHelper(this);
+
+        // Link Java variables with XML IDs
+        etType = findViewById(R.id.etWorkoutName);
+        etValue = findViewById(R.id.etWorkoutValue);
+        btnSave = findViewById(R.id.btnSaveWorkout);
+    }
 }
