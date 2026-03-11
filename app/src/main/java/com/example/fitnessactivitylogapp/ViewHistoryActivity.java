0581134408adc23e69004b9dbb9bfdd881a03a2c
yathursana <yathusanah@gmail.com>
2026-03-11 20:44:54 +0530

    Setup ViewHistoryActivity UI and navigation logic

--- app/src/main/java/com/example/fitnessactivitylogapp/ViewHistoryActivity.java
+++ app/src/main/java/com/example/fitnessactivitylogapp/ViewHistoryActivity.java
@@ -1,4 +1,39 @@
 package com.example.fitnessactivitylogapp;
 
-public class ViewHistoryActivity {
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewHistoryActivity extends AppCompatActivity {
+    ListView lvWorkoutHistory;
+    DatabaseHelper db;
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_history_page);
+
+        // UI components initialization
+        lvWorkoutHistory = findViewById(R.id.lvWorkoutHistory);
+        btnBack = findViewById(R.id.btnBack);
+        db = new DatabaseHelper(this);
+
+        // Close activity when Back button is clicked
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
